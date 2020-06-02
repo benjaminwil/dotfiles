7405b78525f8a3560013ae391b507ccb7a7dfc06
Benjamin Willems <bw@benjaminwil.info>
2020-06-02 10:21:36 -0700

    Get more comfortable with initial emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,6 +3,7 @@
 (setq inhibit-startup-screen t)
 (setq custom-file (expand-file-name "custom.el" base-dir))
 (setq backup-directory-alist `(("." . "~/.emacs.d/temp")))
+(setq create-lockfiles nil)
 
 (when (display-graphic-p)
   (load-theme 'deeper-blue))
@@ -15,7 +16,9 @@
 
 (setq package-list '(counsel
                     evil
+                    flx
                     ivy
+                    magit
                     projectile))
 
 (dolist (pkg package-list)
@@ -24,9 +27,14 @@
 
 (require 'evil)
 
+(global-display-line-numbers-mode 1)
+
+(if (string-match-p (buffer-name) "*")
+  (display-line-numbers-mode 0))
+
 (counsel-mode 1)
 (evil-mode 1)
-(global-display-line-numbers-mode 1)
+(display-line-numbers-mode 1)
 (ivy-mode 1)
 (show-paren-mode 1)
 
@@ -39,13 +47,18 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
 
-(setq ivy-use-virtual-buffers t)
+(setq ivy-re-builders-alist
+      '((t . ivy--regex-fuzzy)))
+
 (setq enable-recursive-minibuffers t)
+(setq ivy-initial-inputs-alist nil)
+(setq ivy-use-virtual-buffers t)
 (setq show-paren-delay 0)
 
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c C-g") 'counsel-git)
+(global-set-key (kbd "M-x") 'counsel-M-x)
 
 (when (eq system-type 'darwin)
  (load-file (expand-file-name "macos.el" base-dir)))
