b76096afd07477ddb95d133c04be9eb33eff8f11
Benjamin Willems <bw@benjaminwil.info>
2020-08-13 20:14:44 -0700

    Tweak bash prompt output, add new leader character

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -2,8 +2,8 @@ source ~/.config/bash/ansi.sh
 
 PROMPT_COLOR=${ANSI_COLOR_BLUE}
 
-ssh_info() {
-    [[ "$SSH_CONNECTION" != '' ]] && echo '%m ' || echo ''
+current_dir() {
+  basename $PWD
 }
 
 git_info() {
@@ -15,6 +15,7 @@ git_info() {
     if [ -n "$GIT_BRANCH" ]; then
         if [[ `git status --short` != '' ]]; then
             local GIT_COLOR=${ANSI_COLOR_YELLOW}
+            local GIT_DISPLAY=${GIT_BRANCH}
             local GIT_CHANGES='*'
         else
             local GIT_DISPLAY=${GIT_BRANCH}
@@ -25,18 +26,22 @@ git_info() {
 
     if [ -d "$GIT_DIR/rebase-merge" ]; then
         local GIT_COLOR=${ANSI_COLOR_RED}
-        local GIT_REBASE_MSG="rebase"
+        local GIT_DISPLAY="rebase"
     fi
 
     if [ -d "$GIT_DIR/rebase-apply" ]; then
         local GIT_COLOR=${ANSI_COLOR_RED}
-        local GIT_REBASE_MSG="rebase-apply"
+        local GIT_DISPLAY="rebase-apply"
     fi
 
-    echo " ${GIT_COLOR}${GIT_REBASE_MSG}${GIT_DISPLAY}${GIT_CHANGES}"
+    echo " ${GIT_COLOR}${GIT_DISPLAY}${GIT_CHANGES}"
+}
+
+ssh_info() {
+    [[ "$SSH_CONNECTION" != '' ]] && echo '%m ' || echo ''
 }
 
 PS1="
-\[$(ssh_info)\]\[${PROMPT_COLOR}\]\[\W\]\[$(git_info)\]
-\[${PROMPT_COLOR}\]⚫\[${ANSI_COLOR_NORMAL}\]"
+\[$(ssh_info)\]\[${PROMPT_COLOR}\]\[\$(current_dir)\]\[\$(git_info)\]
+\[${PROMPT_COLOR}\]⅋ \[${ANSI_COLOR_NORMAL}\]"
 
