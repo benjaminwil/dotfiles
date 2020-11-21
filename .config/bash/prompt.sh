d61aa8a28f2b37a2a1da3e8bca87971a8c0e35f0
benjamin wil <benjamin@super.gd>
2020-11-21 12:09:09 -0800

    If at a git tag, show git tag in prompt
    This is now shown instead of the git branch or commit hash. If there's a
    tag it takes precedence.

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -11,6 +11,7 @@ git_info() {
     local GIT_COLOR="${ANSI_COLOR_NORMAL}"
     local GIT_COMMIT=$(git rev-parse --short HEAD 2> /dev/null)
     local GIT_DIR=$(git rev-parse --git-dir 2> /dev/null)
+    local GIT_TAG=$(git describe --tags 2> /dev/null)
 
     if [ -n "$GIT_BRANCH" ]; then
         if [[ `git status --short` != '' ]]; then
@@ -20,6 +21,9 @@ git_info() {
         else
             local GIT_DISPLAY=${GIT_BRANCH}
         fi
+    elif [ -n "$GIT_TAG" ]; then
+        local GIT_COLOR=${ANSI_COLOR_GREEN}
+        local GIT_DISPLAY=${GIT_TAG}
     else
         local GIT_DISPLAY=${GIT_COMMIT}
     fi
