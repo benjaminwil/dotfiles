0e14ddcc034cc4f49367b7cc5961371f29294ea6
benjamin wil <benjamin@super.gd>
2023-10-18 15:29:35 -0700

    Source Homebrew on macOS-running devices
    Oops? Not sure how I've lived without this at work for so long. My
    computer was totally nonfunctional without these lines this morning.
    I had to use VIM and ZSH! I was in HELL!

--- .config/macos/global.sh
+++ .config/macos/global.sh
@@ -1,3 +1,5 @@
+[[ -f /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
+[[ -f /usr/local/bin/brew ]] && eval $(/usr/local/bin/brew shellenv)
 
 if [ -f `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
     . `brew --prefix`/etc/bash_completion.d/git-completion.bash
