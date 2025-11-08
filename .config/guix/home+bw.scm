4709f66eb3bd84007767488c9fe29e6e120c74f0
benjamin wil <hey@benjaminwil.info>
2025-11-07 18:34:12 -0800

    Add initial Guix Home configuration

---
+++ .config/guix/home+bw.scm
@@ -0,0 +1,35 @@
+(define-module (guix-home-config)
+  #:use-module (gnu home)
+  #:use-module (gnu home services)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages gnupg)
+  #:use-module (gnu packages gnuzilla)
+  #:use-module (gnu packages messaging)
+  #:use-module (gnu packages password-utils)
+  #:use-module (gnu packages rust-apps)
+  #:use-module (gnu packages shellutils)
+  #:use-module (gnu packages ssh)
+  #:use-module (gnu packages text-editors)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu services)
+  #:use-module (gnu home services desktop)
+  #:use-module (gnu home services syncthing)
+  #:use-module (gnu system shadow))
+
+(home-environment
+  (packages (list fzy
+                  git
+                  gnupg
+                  htop
+                  icedove
+                  kakoune
+                  kak-lsp
+                  nnn
+                  openssh
+                  password-store
+                  ripgrep
+                  senpai))
+  (services (list
+             (service home-syncthing-service-type
+                      (for-home (syncthing-configuration (user "bw")))))))
