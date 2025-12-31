c76bef5ccdbadd799bd71063598137fe87fb7099
benjamin wil <hey@benjaminwil.info>
2025-12-30 17:37:45 -0700

    Add fallbacks to set `BUNDLE_JOBS` dynamically

--- .config/bundler/global.sh
+++ .config/bundler/global.sh
@@ -1,4 +1,4 @@
 export BUNDLE_DISABLE_LOCAL_BRANCH_CHECK=true
 export BUNDLE_PATH=vendor/bundle
 export BUNDLE_DISABLE_SHARED_GEMS=1
-export BUNDLE_JOBS=$(getconf _NPROCESSORS_ONLN)
+export BUNDLE_JOBS=$(getconf _NPROCESSORS_ONLN 2>/dev/null || nproc --all 2>/dev/null || grep -c ^processor /proc/cpuinfo)
