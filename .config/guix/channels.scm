af9a9c8d33ecc32ff6892aa43fb74f3bf0ef3cf8
benjamin wil <hey@benjaminwil.info>
2026-01-24 14:50:30 -0800

    Update Guix channels

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -18,7 +18,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "a6376bff79bfccc5e9519cf20954a89197884fac")
+          "f5338f63fce69622ce06f93fe02524967e1f30d4")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -30,7 +30,7 @@
         (url "https://codeberg.org/guix/guix.git")
         (branch "master")
         (commit
-          "51d42cd40fb7248f43566fa03e9f8771f902e45e")
+          "6a62ff0170a5deaa4f0f5a6e2e51cd4ac33dc513")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
